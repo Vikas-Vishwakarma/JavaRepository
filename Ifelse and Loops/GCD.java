10aab24f7d450b2e5b9f433a43eefca76bf5cb46
Vikas Vishwakarma <vikaspvishwakarma@gmail.com>
2022-08-26 21:52:35 +0530

    Java program to find GCD of Two number

---
+++ Ifelse and Loops/GCD.java
@@ -0,0 +1,23 @@
+package com.gfg.basic.java;
+
+import java.util.Scanner;
+
+public class GCD {
+//	public static void main(String[] args) {
+//		int gcd = 0;
+//		 Scanner sc = new Scanner(System.in);
+//		 System.out.println("Enter two numbers to find GCD: ");
+//		 int a = sc.nextInt();
+//		 int b = sc.nextInt();		 
+//		 sc.close();
+//		 
+//		 for(int i = 1;  i <= a && i <= b; i++) {
+//			 if(a % i == 0 && b % i == 0) {
+//				 gcd = i;
+//			 }
+//		 }
+//		 System.out.println("GCD: "+gcd);
+//	}	
+
+
+}
